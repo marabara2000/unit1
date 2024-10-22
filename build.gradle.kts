ef302e878028199362a7d1d5a20d8e002c57fdde
Marina B <balda558@gmail.com>
2024-10-22 18:37:06 +0300

    Add test

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,21 @@ repositories {
     mavenCentral()
 }
 
+//dependencies {
+//    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+//}
+//
+//tasks.test {
+//    useJUnitPlatform()
+//}
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation ("org.testng:testng:7.1.0")
+
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useTestNG()
 }
+
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+
+    public void testRemain() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual,expected);
+
+    }
+}
